8434baba9ace55b48de542b91d2758d5ca7273c3
Steven E. Newton <cratermoon@gmail.com>
2016-08-31 09:43:22 -0700

    Removed excessively chatty output from ex3_nn.m

--- assignments/machine-learning-ex3/ex3/ex3_nn.m
+++ assignments/machine-learning-ex3/ex3/ex3_nn.m
@@ -75,14 +75,14 @@ rp = randperm(m);
 
 for i = 1:m
     % Display 
-    fprintf('\nDisplaying Example Image\n');
-    displayData(X(rp(i), :));
+    %fprintf('\nDisplaying Example Image\n');
+    %displayData(X(rp(i), :));
 
     pred = predict(Theta1, Theta2, X(rp(i),:));
-    fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
+    % fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
     
     % Pause
-    fprintf('Program paused. Press enter to continue.\n');
-    pause;
+    %fprintf('Program paused. Press enter to continue.\n');
+    %pause;
 end
 
