cd9534c253915e1e31be2f8c5881f9d33eac902e
Steven E. Newton <cratermoon@gmail.com>
2016-09-26 11:38:03 -0700

    working on dataset3 portion of week7 ex6

--- assignments/machine-learning-ex6/ex6/dataset3Params.m
+++ assignments/machine-learning-ex6/ex6/dataset3Params.m
@@ -2,7 +2,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
 %   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
@@ -23,8 +23,31 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% C and sigma over [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+o = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+predictionError = 1;
+for testC = o
+  for testSigma = o
+    % TODO: tolerance and max_passes ?
+    model = svmTrain(X, y, testC, @(x1, x2) gaussianKernel(x1, x2, testSigma));
+    predictions = svmPredict(model, Xval);
+    newPredictionError = mean(double(predictions ~= yval));
+    if newPredictionError < predictionError
+      C = testC;
+      sigma = testSigma;
+      predictionError = newPredictionError;
+    endif
+  end
+end
 
-
+%!test
+%! X = [0.011521, -0.657895; -0.097120, 0.213158; -0.352189, 0.258187; -0.215323, 0.295029; 0.143433, 0.168129];
+%! y = [0;1;1;1;0];
+%! Xval = [0.118548, 0.192690; -0.016244, -0.167544; -0.099078, -0.500000; -0.108295, -0.084795; -0.435484, 0.160819];
+%! yval = [0;0;0;0;1];
+%! [C, sigma] = dataset3Params(X, y, Xval, yval)
+%! assert(C, 0.01);
+%! assert(sigma, 0.03);
 
 
 
