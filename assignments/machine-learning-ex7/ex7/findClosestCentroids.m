f756b93bd73fce40b76d1cbca8e2fce333bbb792
Steven E. Newton <cratermoon@gmail.com>
2016-09-28 09:46:46 -0700

    week 8 exercise 7 part 1: Find Closest Centroids (k-Means) complete

--- assignments/machine-learning-ex7/ex7/findClosestCentroids.m
+++ assignments/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,12 +15,33 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
+n = length(X)
+for i=1:n
+  j = norm(X(i,:) - centroids(1,:))^2;
+  idx(i) = 1;
+  for k=1:K
+    new_j = norm(X(i,:) - centroids(k,:))^2;
+    if (new_j <= j)
+      idx(i) = k;
+      j = new_j;
+    end
+  end
+end
+%!test
+%! centroids = [3,3; 6,2; 8,5];
+%! X = [1.8421, 4.6076; 5.6586, 4.8000; 6.3526,3.2909];
+%! idx = findClosestCentroids(X, centroids);
+%! assert(idx, [1;3;2]);
+%!test
+%! X = reshape(sin(1:50), 10, 5);
+%! cent = X(7:10,:)
+%! idx = findClosestCentroids(X, cent)
+%! assert(idx, [1; 2; 3; 4; 4; 1; 1; 2; 3; 4]);
 
 
 
@@ -30,4 +51,3 @@ idx = zeros(size(X,1), 1);
 % =============================================================
 
 end
-
