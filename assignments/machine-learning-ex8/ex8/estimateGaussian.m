0ef51727bbcac410b0e6e101baf738841b6b9541
Steven E. Newton <cratermoon@gmail.com>
2016-10-07 17:42:13 -0700

    Week 9 ex 8 part 1 mostly done

--- assignments/machine-learning-ex8/ex8/estimateGaussian.m
+++ assignments/machine-learning-ex8/ex8/estimateGaussian.m
@@ -1,11 +1,11 @@
 function [mu sigma2] = estimateGaussian(X)
-%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%ESTIMATEGAUSSIAN This function estimates the parameters of a
 %Gaussian distribution using the data in X
-%   [mu sigma2] = estimateGaussian(X), 
+%   [mu sigma2] = estimateGaussian(X),
 %   The input X is the dataset with each n-dimensional data point in one row
 %   The output is an n-dimensional vector mu, the mean of the data set
 %   and the variances sigma^2, an n x 1 vector
-% 
+%
 
 % Useful variables
 [m, n] = size(X);
@@ -21,16 +21,18 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
-
-
-
-
-
+mu = (1/m) * sum(X);
+sigma2 = (1/m) * sumsq(X-mu);
 
 % =============================================================
 
 
 end
+%!test
+%! X = sin(magic(4));
+%! X = X(:,1:3);
+%! [mu sigma2] = estimateGaussian(X);
+%! assert(size(mu), [1,3])
+%! assert(size(sigma2), [1,3])
+%! assert(mu, [-0.3978779, 0.3892253, -0.0080072], 0.0002)
+%! assert(sigma2, [0.27795, 0.65844, 0.20414], 0.0002)
