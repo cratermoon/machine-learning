f33fa7dd62b7d328c2a65c549bb73a5ef84e8400
Steven E. Newton <cratermoon@gmail.com>
2016-10-09 19:25:50 -0700

    added bonus test to exercise 8 cofiCostFunc

--- assignments/machine-learning-ex8/ex8/cofiCostFunc.m
+++ assignments/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -78,3 +78,16 @@ end
 %! [J grad] = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda);
 %! assert(J,342.08, 0.01)
 %! assert(grad, [-15.5880; -22.3440; -3.3590; -12.5720; -18.4380; -26.8620; -2.3660; -14.7440; 1.9770; -1.0280; 3.1860; -5.0590; -8.2600; -1.3420], 0.002)
+%!test
+% a user with no reviews
+%! params = [ 1:14 ] / 10;
+%! Y = magic(4);
+%! Y = Y(:,1:3);
+%! R = [1 0 1; 1 1 1; 0 0 0; 1 1 0] > 0.5;     % R is logical
+%! num_users = 3;
+%! num_movies = 4;
+%! num_features = 2;
+%! lambda = 6;
+%! [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda);
+%! assert(J,331.08, 0.01)
+%! assert(grad, [-15.5880; -22.3440; 1.8000; -12.5720; -18.4380; -26.8620; 4.2000; -14.7440; 1.9770; -1.0280; 4.5930; -5.0590; -8.2600; 1.9410], 0.002)
