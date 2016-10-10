b7650feb188a17e416bc2866c44eff960b087beb
Steven E. Newton <cratermoon@gmail.com>
2016-10-09 18:38:35 -0700

    Week 9 Exercise 8 Collaborative Filtering Cost done

--- assignments/machine-learning-ex8/ex8/cofiCostFunc.m
+++ assignments/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -11,7 +11,6 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -21,7 +20,7 @@ Theta_grad = zeros(size(Theta));
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
+%               matches our costs. After that, you should implement the
 %               gradient and use the checkCostFunction routine to check
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
@@ -29,18 +28,19 @@ Theta_grad = zeros(size(Theta));
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the
 %            i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
+%        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        Theta_grad - num_users x num_features matrix, containing the
 %                     partial derivatives w.r.t. to each element of Theta
 %
-
-
+%thetaTransX = X*Theta'
+%e = R.*(thetaTransX - Y)
+J = (1/2) * sum(sumsq(((X*Theta') - Y).*R));
 
 
 
@@ -60,3 +60,15 @@ Theta_grad = zeros(size(Theta));
 grad = [X_grad(:); Theta_grad(:)];
 
 end
+
+%!test
+%! params = [ 1:14 ] / 10;
+%! Y = magic(4);
+%! Y = Y(:,1:3);
+%! R = [1 0 1; 1 1 1; 0 0 1; 1 1 0] > 0.5;     % R is logical
+%! num_users = 3;
+%! num_movies = 4;
+%! num_features = 2;
+%! lambda = 0;
+%! [J grad] = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda);
+%! assert(J,311.63, 0.01)
